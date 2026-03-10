444e095a9c494cc54fa81743cafc457da1a4f5dc
Divya Dharshini . M <divyadharshinimagesh0308_bec28@mepcoeng.ac.in>
2026-03-10 15:32:24 +0530

    Create happiness code.py

---
+++ happiness code.py
@@ -0,0 +1,11 @@
+m, n=map(int, input().split())
+array=list(map(int, input().split()))
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))
+happiness = 0
+for x in array:
+    if x in a:
+        happiness += 1
+    elif x in b:
+        happiness -= 1
+print(happiness)
